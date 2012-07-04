93fe9694ff13f8f7336b2f9996f9e4134a0b08cc
matiassbb <matiassbb@gmail.com>
2012-07-04 16:07:09 +0000

    Se agrega link "goHome" Se modifica logica para que muestre la homePage sin necesidad de estar registrado

--- fesf/fesf-interface/src/main/scala/ar/edu/fesf/scala/view/pages/SignInLdap.scala
+++ fesf/fesf-interface/src/main/scala/ar/edu/fesf/scala/view/pages/SignInLdap.scala
@@ -3,9 +3,13 @@ package ar.edu.fesf.scala.view.pages
 import java.io.Serializable
 import org.apache.wicket.markup.html.WebPage
 import org.apache.wicket.authroles.authentication.panel.SignInPanel
+import org.apache.wicket.markup.html.link.BookmarkablePageLink
+import org.apache.wicket.protocol.http.WebApplication
+import ar.edu.fesf.scala.view.application.MyApplication
+
 
 class SignInLdap extends WebPage with Serializable {
-  
-  this.add(new SignInPanel("signIn"))
 
+  this.add(new SignInPanel("signIn"))
+  this.add(new BookmarkablePageLink("goHome", this.getApplication().getHomePage()))
 }
